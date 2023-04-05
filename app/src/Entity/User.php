702136980c2ff8a32614737a74366bb0bb822ac3
Olexa Petrenko <petrenkodesign@gmail.com>
2023-04-05 03:43:37 +0300

    added minimal authorization functionality

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -34,6 +34,16 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="blob")
+     */
+    private $approved;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -113,5 +123,29 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getApproved()
+    {
+        return $this->approved;
+    }
+
+    public function setApproved($approved): self
+    {
+        $this->approved = $approved;
+
+        return $this;
     }
 }
